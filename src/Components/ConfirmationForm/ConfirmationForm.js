c959c43e64355732d68713a0051f6ffc4b20ef8e
falcaomm <manu.m.falcao@gmail.com>
2023-01-31 09:59:16 -0300

    inputs controlados ex

--- src/Components/ConfirmationForm/ConfirmationForm.js
+++ src/Components/ConfirmationForm/ConfirmationForm.js
@@ -1,13 +1,61 @@
 import React from 'react'
-import { Form} from '../MainPage/styles'
+import { Form, Input } from '../MainPage/styles'
 
 const ConfirmationForm = () => {
   return (
     <Form>
-        
-      <button>Enviar dados</button>
-      </Form>
-  )
+      <label>
+        Data Nascimento:
+        <Input
+          placeholder='Data Nascimento'
+          type={'date'}
+        />
+      </label>
+
+      <label>
+        Telefone:
+        <Input
+          placeholder='Telefone'
+          type={'tel'}
+        />
+      </label>
+
+      <label>
+        Genero:
+        <select name="select">
+          <option value="valor1">Valor 1</option>
+          <option value="valor2" selected>Valor 2</option>
+          <option value="valor3">Valor 3</option>
+        </select>
+      </label>
+
+      <label>
+        Autodeclarado:
+        <select name="select">
+          <option value="valor1">Valor 1</option>
+          <option value="valor2" selected>Valor 2</option>
+          <option value="valor3">Valor 3</option>
+        </select>
+        </label>
+
+        <label>
+          Curso integral web fullstack
+          <Input type={'radio'}
+          />
+        </label>
+
+        <label>
+          Curso noturno web fullstack
+          <Input type={'radio'}
+          />
+        </label>
+
+        <input type="checkbox" />
+        <label for="vehicle1"> I have a bike</label>
+
+          <button>Enviar dados</button>
+        </Form>
+        )
 }
 
-export default ConfirmationForm
+        export default ConfirmationForm
--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,36 +1,61 @@
-import React, {useState} from 'react'
-import {MainContainer} from './styles'
+import React, { useState } from 'react'
+import { MainContainer } from './styles'
 import NameForm from '../NameForm/NameForm'
 import ConfirmationForm from '../ConfirmationForm/ConfirmationForm'
 
 const MainPage = () => {
-const [name, setName] = useState("")
-const [age, setAge] = useState("")
-const [email, setEmail] = useState("")
-const [formFlow, setFormFlow] = useState(1) //você não precisa mexer neste estado, ele faz parte da lógica da linha 30 do JSX
+  const [name, setName] = useState("")
+  const [age, setAge] = useState("")
+  const [email, setEmail] = useState("")
+  const [email2, setEmail2] = useState("")
+  const [formFlow, setFormFlow] = useState(1)
 
-const onChangeName = (event) => {
-  setName(event.target.value)
-}
+  const onChangeName = (event) => {
+    setName(event.target.value)
+  }
 
-const onChangeAge = (event) => {
-  setAge(event.target.value)
-}
+  const onChangeAge = (event) => {
+    setAge(event.target.value)
+  }
 
-const onChangeEmail = (event) => {
-  setEmail(event.target.value)
-}
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value)
+  }
 
-const sendData = () => {
-  //aqui deve vir uma verificação para mudar de formulario apenas se todos os requisitos tiverem sido cumpridos
-  setFormFlow(2)
-}
+  const onChangeEmail2 = (event) => {
+    setEmail2(event.target.value)
+  }
+
+
+  // const sendData = () => {
+  //   if ( age > 18 && name.length < 31 && name.length > 9 && email.includes("@") && email === email2) {
+  //     setFormFlow(2)
+  //   } else{
+  //     alert("Os dados não foram preenchidos corretamente")
+  //   }
+  // }
+
+  const sendData = () => {
+      setFormFlow(2)
+  }
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
-      {formFlow === 1 ? <NameForm
-      // insira aqui suas props
-      /> : <ConfirmationForm />}
+      {formFlow === 1 ?
+        <NameForm
+          name={name}
+          age={age}
+          email={email}
+          email2={email2}
+          formFlow={formFlow}
+          onChangeName={onChangeName}
+          onChangeAge={onChangeAge}
+          onChangeEmail={onChangeEmail}
+          onChangeEmail2={onChangeEmail2}
+          sendData={sendData}
+        />
+        : <ConfirmationForm />}
     </MainContainer>
   )
 }
